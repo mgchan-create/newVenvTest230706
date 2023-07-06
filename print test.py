1472dd749cbe2bed4650b89815dee0d8759f2c86
sy24 <sy24drg@gmail.com>
2023-07-06 16:49:56 +0900

    test2

--- print test.py
+++ print test.py
@@ -7,11 +7,9 @@ fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 streamhandler = logging.StreamHandler()
 streamhandler.setFormatter(fmt)
-logs.addHandler(streamhandler)
-logs.info("......")
+# logs.addHandler(streamhandler)
+# logs.info("......")
 
-
-
-a = [1,2,3,4,5,6,7,8,9,10]
-print(a,sep = '\n', end ='\n')
-print(*a,sep = '\n', end ='')
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(a, sep='\n', end='\n')
+print(*a, sep='\n', end='')
