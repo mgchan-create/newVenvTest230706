d0180451a688b84ec1b0509b6e0445046e4be29a
sy24 <sy24drg@gmail.com>
2023-07-06 16:40:10 +0900

    test

---
+++ print test.py
@@ -0,0 +1,17 @@
+import logging
+
+logs = logging.getLogger()
+logs.setLevel(logging.INFO)
+
+fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+streamhandler = logging.StreamHandler()
+streamhandler.setFormatter(fmt)
+logs.addHandler(streamhandler)
+logs.info("......")
+
+
+
+a = [1,2,3,4,5,6,7,8,9,10]
+print(a,sep = '\n', end ='\n')
+print(*a,sep = '\n', end ='')
